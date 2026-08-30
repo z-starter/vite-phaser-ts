02da4e09dfad1e428165d8253f03fb5e3e5d9e5e
Zero <zero.red.dev@gmail.com>
2026-08-30 13:06:43 +0330

    Update project to support new changes

--- src/main.ts
+++ src/main.ts
@@ -5,19 +5,19 @@ import { GameOver } from "./scenes/GameOver"
 import { MainMenu } from "./scenes/MainMenu"
 import { Preloader } from "./scenes/Preloader"
 
-import { Game, Types } from "phaser"
+import { Game, Types, AUTO, Scale } from "phaser"
 
 //  Find out more information about the Game Config at:
 //  https://newdocs.phaser.io/docs/3.70.0/Phaser.Types.Core.GameConfig
 const config: Types.Core.GameConfig = {
-  type: Phaser.AUTO,
+  type: AUTO,
   width: 1024,
   height: 768,
   parent: "game-container",
   backgroundColor: "#028af8",
   scale: {
-    mode: Phaser.Scale.FIT,
-    autoCenter: Phaser.Scale.CENTER_BOTH,
+    mode: Scale.FIT,
+    autoCenter: Scale.CENTER_BOTH,
   },
   scene: [Boot, Preloader, MainMenu, MainGame, GameOver],
 }
